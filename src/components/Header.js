1c8c9ebebd42943b15fdeed0727c0334cc2c04d4
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2021-01-12 17:34:38 +0530

    Index2

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useEffect,useState } from "react"
 import {AppBar, Collapse, IconButton, makeStyles, Toolbar} from "@material-ui/core"
 import SortIcon from "@material-ui/icons/Sort"
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
+import "./Header.css";
 
 const useStyles = makeStyles((theme)=>({
     root :{
@@ -10,7 +10,8 @@ const useStyles = makeStyles((theme)=>({
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        color: "white"
+        color: "white",
+        fontFamily: 'Lobster'
     },
     appbar :{
         background: 'none',
@@ -30,14 +31,15 @@ const useStyles = makeStyles((theme)=>({
     text2:{
         fontFamily: "Lobster",
         textAlign: "center",
-        fontSize: "4rem",
+        fontSize: "3.5rem",
+        paddingTop: "20rem"
     },
     text2Container:{
         textAlign: "center",
     },
     iconDown:{
         color:"#0099ff",
-        fontSize: "4rem"
+        fontSize: "3.5rem"
     }
 
 }))
@@ -50,7 +52,7 @@ export default function Header() {
         <div className={classes.root}>
             <AppBar className={classes.appbar} elevation={0}>
                 <Toolbar className={classes.appbarToolbar}>
-                <h1 className={classes.appbarTitle}>My <span className={classes.textColor}>Page</span></h1>
+                <h1 className={classes.appbarTitle}><span style={{fontFamily: "Lobster", fontSize:"1.5rem"}}>My <span className={classes.textColor}>Page</span></span></h1>
                 <IconButton>
                     <SortIcon style={{ color: "white", fontSize: '2rem'}}/>
                 </IconButton>
@@ -59,7 +61,7 @@ export default function Header() {
             <Collapse in={checked} {...(checked ? {timeout: 1500}:{})} collapsedHeight={40}>
                 <div className={classes.text2Container}>
                     <h1 className={classes.text2}>
-                        Welcome to<br /> My <span className={classes.textColor}>Website</span>
+                        Welcome to<br/>My <span className={classes.textColor}>Website</span>
                     </h1>
                     <IconButton>
                         <ExpandMoreIcon className={classes.iconDown} />
