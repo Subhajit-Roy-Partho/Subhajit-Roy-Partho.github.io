f96d4c406fce3976dc9072e9cabb657f5b9d3dec
SubhajitRoyPartho <me.subhajitroy1999@gmail.com>
2022-09-25 07:27:53 +0530

    upload

--- src/components/mdxComponent.js
+++ src/components/mdxComponent.js
@@ -2,22 +2,22 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 
 export function BoxHighlight({input}){
-    // if(!(input.color)){
-    //     input.color = "#DEEBFF"
-    // }
-    console.log(input)
+    if(input['color'] === undefined){
+        input['color']="#DEEBFF"
+    }
+    // console.log(input['_input'])
     return(
         <Box
         sx={{
             width: "90%",
-            backgroundColor: '#DEEBFF',
+            backgroundColor: input['color'],
             borderRadius: '1vh',
             paddingTop: '0.002vh',
             paddingBottom: '0.002vh',
             paddingLeft: '1%',
             paddingRight: '1%',
         }}>
-            <p>{input.text}</p>
+            <p>{input['text']}</p>
         </Box>
     );
 }
