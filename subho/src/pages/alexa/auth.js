a1746253bb58cde4801adb2a18b123493de15ac7
Subhajit Roy <40741607+Subhajit-Roy-Partho@users.noreply.github.com>
2023-02-21 19:31:44 -0700

    update

--- subho/src/pages/alexa/auth.js
+++ subho/src/pages/alexa/auth.js
@@ -1,9 +1,15 @@
 import * as React from "react";
+import * as queryString from 'query-string;';
+
 
 export default function Index(){
+    var userId = queryString.parse(location.search);
     return(
         <div>
             <h1>Developing</h1>
+            <p>
+                The userID is : {userId}
+            </p>
         </div>
     )
 }
