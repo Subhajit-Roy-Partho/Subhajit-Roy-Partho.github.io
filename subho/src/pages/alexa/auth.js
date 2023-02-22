0350bcbe7dee52929d4a3779ef045768bfa3b5a6
SubhajitRoyPartho <me.subhajitroy1999@gmail.com>
2023-02-21 20:31:40 -0700

    corrected

--- subho/src/pages/alexa/auth.js
+++ subho/src/pages/alexa/auth.js
@@ -4,18 +4,12 @@ import * as React from "react";
 export default function Index(props){
     var string = props.location.search;
     string = string.split("?");
-    var userId = string[1].substring(6);
-    // var data = string[2].split("data=").splice(1);
-    console.log(userId);
     return(
         <div>
             <h1>Developing</h1>
             <p>
-                The userID is : {userId}
+                The userID is : {string[1]}
             </p>
-            {/* <p>
-                The Data is: {data}
-            </p> */}
         </div>
     )
 }
