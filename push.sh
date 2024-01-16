e28b360221eb062046d845fa4a745ac9b684ddad
Subhajit Roy <me.subhajitroy1999@gmail.com>
2024-01-15 17:03:26 -0700

    update

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 npm install
 bundle
-bundle exec jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 ssh sroy85@general.asu.edu 'rm -rf /afs/asu.edu/users/s/r/o/sroy85/www/*'
 rsync -rzvP _site/* sroy85@general.asu.edu:/afs/asu.edu/users/s/r/o/sroy85/www
